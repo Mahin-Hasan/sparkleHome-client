2b7d8f9566928085c9aff6fd6e6435674a9aa983
Mahin Hasan <mahinmtrs@gmail.com>
2024-06-06 12:37:00 +0600

    verified logged user email with token email

--- sparklehome-server/index.js
+++ sparklehome-server/index.js
@@ -3,10 +3,13 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const secret = 'willBeUsedInJwtToken'
 
+//parsers
 app.use(express.json())
+app.use(cookieParser())
 const port = 3000
 
 
@@ -30,10 +33,32 @@ async function run() {
         const serviceCollection = client.db('sparkleHome').collection('services')
         const bookingCollection = client.db('sparkleHome').collection('bookings')
 
+        //maddleware
+        //verrify token and grant access
+        const gateman = (req, res, next) => {
+            const { token } = req.cookies //also be written as cookies.token
+
+            if (!token) {
+                return res.status(401).send({ message: 'Unauthorized access' })
+            }
+
+            //verify a token symmetric
+            jwt.verify(token, secret, function (err, decoded) {
+                if (err) {
+                    return res.status(401).send({ message: 'Unauthorized access' })
+                }
+                console.log(decoded);
+                //attach decoded user so that others can get it
+                req.user = decoded;
+
+                next()
+            });
+        }
+
         app.get('/', (req, res) => {
             res.send('SparkleHome is running !!!')
         })
-        app.get('/api/v1/services', async (req, res) => {
+        app.get('/api/v1/services', gateman, async (req, res) => {
             const cursor = serviceCollection.find()
             const result = await cursor.toArray()
             res.send(result)
@@ -43,6 +68,17 @@ async function run() {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking)
             res.send(result)
+        })
+        //user specific bookings
+        app.get('/api/v1/user/bookings', gateman, async (req, res) => {
+            const queryEmail = req.query.email;
+            const tokenEmail = req.user.email;
+            //match user email to check if it is a valid user
+            console.log(queryEmail, tokenEmail);
+            if (queryEmail === tokenEmail) {
+                const result = await bookingCollection.findOne({ email: queryEmail })
+                res.send(result)
+            }
         })
 
         app.delete('/api/v1/user/cancel-booking/:bookingId', async (req, res) => {
@@ -56,9 +92,12 @@ async function run() {
             //Creating token and send to client
 
             const user = req.body
-            const token = jwt.sign(user, secret)
-            console.log(token);
-            // jwt.sign({ payload,  },secret)
+            const token = jwt.sign(user, secret, { expiresIn: 60 * 60 })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: 'none'
+            }).send({ success: true })
         })
 
         // Send a ping to confirm a successful connection
