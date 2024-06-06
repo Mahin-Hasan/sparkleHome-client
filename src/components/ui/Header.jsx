2a24bcd2fc6fca1888033a650af4fa89d8dea7a2
Mahin Hasan <mahinmtrs@gmail.com>
2024-06-06 14:40:21 +0600

    designed services

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = ({ title, children }) => {
+    return (
+        <div className="flex flex-col items-center w-full my-24">
+            <h1 className="text-4xl font-semibold">{title}</h1>
+            <p className="max-w-[75ch] text-center mt-5 mb-10">{children}</p>
+        </div>
+    );
+};
+
+
+
+export default Header;
